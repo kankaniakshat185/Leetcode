a2f3739608ab5ff290fefa605ef2dca1c0cf3b58
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-06-24 19:26:17 +0530

    Time: 3 ms (65.04%) | Memory: 8.4 MB (10.95%) - LeetSync

--- leetcode/1013-fibonacci-number/fibonacci-number.cpp
+++ leetcode/1013-fibonacci-number/fibonacci-number.cpp
@@ -3,7 +3,7 @@ public:
     int solve(int n, vector<int>& dp){
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
-        return solve(n-1, dp)+solve(n-2, dp);
+        return dp[n] = solve(n-1, dp)+solve(n-2, dp);
     }
     int fib(int n) {
         vector<int> dp(n+1, -1);
