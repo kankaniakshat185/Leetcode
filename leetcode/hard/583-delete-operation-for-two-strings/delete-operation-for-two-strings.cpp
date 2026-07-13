0461800217d7a267ae141e197fab1d19b6bf9b07
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-07-13 09:45:45 +0530

    Time: 12 ms (49.21%) | Memory: 16.8 MB (36.02%) - LeetSync

--- leetcode/hard/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ leetcode/hard/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -3,12 +3,6 @@ public:
     int minDistance(string word1, string word2) {
         int s1=word1.length(), s2=word2.length();
         vector<vector<int>> dp(s1+1, vector<int> (s2+1, 0));
-        for(int i=0;i<s1;i++){
-            dp[i][s2]=0;
-        }
-        for(int i=0;i<s2;i++){
-            dp[s1][i]=0;
-        }
         for(int i=s1-1;i>=0;i--){
             for(int j=s2-1;j>=0;j--){
                 if(word1[i]==word2[j]){
@@ -19,12 +13,6 @@ public:
                 }
             }
         }
-        if(dp[0][0]==s1){
-            return s2-dp[0][0];
-        }
-        else if(dp[0][0]==s2){
-            return s1-dp[0][0];
-        }
-        else return (s1+s2-2*dp[0][0]);
+        return (s1+s2-2*dp[0][0]);
     }
 };
