4f4d3d9e665e41daafd91d71ad8fa153496eb914
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-07-22 21:18:54 +0530

    Time: 395 ms (50.22%) | Memory: 211.4 MB (47.16%) - LeetSync

---
+++ leetcode/medium/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(vector<int>& prices, int n, int i, int cap, bool buy, vector<vector<vector<int>>>& dp){
+        if(i==n) return 0;
+        if(cap==0) return 0;
+        if(dp[i][buy][cap]!=-1) return dp[i][buy][cap];
+
+        if(!buy && cap!=0) return dp[i][buy][cap]=max(solve(prices, n, i+1, cap, true, dp)-prices[i], solve(prices, n, i+1, cap, false, dp));
+        else return dp[i][buy][cap]=max(solve(prices, n, i+1, cap, true, dp), solve(prices, n, i+1, cap-1, false, dp)+prices[i]);
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, 0)));
+        for(int cap=0;cap<3;cap++){
+            dp[n][0][cap]=0;
+            dp[n][1][cap]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int cap=1;cap<3;cap++){
+                dp[i][0][cap]=max(dp[i+1][1][cap]-prices[i], dp[i+1][0][cap]);
+                dp[i][1][cap]=max(dp[i+1][1][cap], dp[i+1][0][cap-1]+prices[i]);
+            }
+        }
+        return dp[0][0][2];
+    }
+};
