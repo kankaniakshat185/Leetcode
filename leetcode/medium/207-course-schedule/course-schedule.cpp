b9d2a5ef1bf48e112eabcdd9f01c239439b2ddca
Akshat Kankani <157486069+kankaniakshat185@users.noreply.github.com>
2026-09-17 16:25:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (69.03%) - LeetSync

---
+++ leetcode/medium/207-course-schedule/course-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=prerequisites.size();
+        vector<vector<int>> adjList(numCourses);
+        vector<int> indeg(numCourses);
+        for(int i=0;i<n;i++){
+            adjList[prerequisites[i][0]].push_back(prerequisites[i][1]);
+            indeg[prerequisites[i][1]]++;
+        }
+        queue<int> q;
+        int count=0;
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+        if(q.empty()) return false;
+        while(!q.empty()){
+            int node=q.front();
+            count++;
+            q.pop();
+
+            for(auto it:adjList[node]){
+                indeg[it]--;
+                if(indeg[it]==0) q.push(it);
+            }
+        }
+        return (numCourses==count);
+    }
+};
